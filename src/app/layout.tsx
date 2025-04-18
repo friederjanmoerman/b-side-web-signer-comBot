aba9f366d6ad9a33d42afa8469f36caa684d51cc
Hardcodepunk <howl@hardcodepunk.be>
2025-04-18 11:09:36 +0530

    update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,17 @@
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
+import "./globals.css"
+import { ReactNode, Suspense } from "react"
+
+export const metadata = {
+  title: "B Side Wallet Verifier",
+  description: "Sign messages securely to verify your B Side NFT ownership",
+}
+
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
+      </body>
     </html>
   )
 }
